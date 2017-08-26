97a882450c431274b5cb449b7a0bc27d2d351370
pirishd <pirishd@icloud.com>
2017-08-26 09:51:29 +0200

    Create basic project

---
+++ InstantRecord/InstantRecord.h
@@ -0,0 +1,19 @@
+//
+//  InstantRecord.h
+//  InstantRecord
+//
+//  Created by Patrick on 26/08/2017.
+//  Copyright © 2017 pirishd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InstantRecord.
+FOUNDATION_EXPORT double InstantRecordVersionNumber;
+
+//! Project version string for InstantRecord.
+FOUNDATION_EXPORT const unsigned char InstantRecordVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstantRecord/PublicHeader.h>
+
+
